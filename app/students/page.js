886c32c7a5aae5999d55ce4581ba307c87d97ea7
vikash0p <vikash2000158gmail.com>
2024-01-13 12:06:06 +0530

    no destucture  student property.

--- app/students/page.js
+++ app/students/page.js
@@ -4,13 +4,14 @@ import Link from 'next/link';
 import React from 'react'
 const Students = async () => {
 
-  const {  student } = await fetchStudentdata();
+  const  student  = await fetchStudentdata();
+  console.log(student);
   
 
   return (
     <div className='container mx-auto my-8 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 '>
       {
-        student.map((value, index, array) => {
+        student?.student?.map((value, index, array) => {
           return (
             <div key={index} className=''>
               <div className='flex flex-col gap-2 bg-slate-600 rounded-lg py-4 text-center text-white'>
